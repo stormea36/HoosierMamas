d7ac5e4f09c0278f57a7ec51708060aa9e06d2f5
stormea36 <stormea36@gmail.com>
2013-10-03 01:30:35 -0400

    first commit

---
+++ app/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('content') 
+
+<div class="test">
+    <p>It's Hammer Time</p>
+    <h3>WOB wob WOB</h3>
+</div>
---
+++ app/views/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Brinker-Loyalty</title>
+  </head>
+
+<body>
+    
+    
+    
+    @yield('content')
+    @include('footer')
+    
+        
+
+
+<!-- Le javascript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+
+
+  </body>
+</html>
